7c3a4b04725021155fdc5403f29b2460949066e9
Lilia <lilia.manukyan@hotmail.com>
2021-12-18 21:09:12 +0400

    initial houses code

---
+++ components/House.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+export default function House(props) {
+  return (
+    <Link href="/houses/[id]" as={'/houses/' + props.id}>
+      <a>
+        <img src={props.picture} width="100%" alt="House picture" />
+        <p>
+          {props.type} - {props.town}
+        </p>
+        <p>{props.title}</p>
+      </a>
+    </Link>
+  );
+}
